17d0a4358c4152a44009209253d0a0095bb74ef3
subodhrana390 <subodhrana390@gmail.com>
2024-01-22 09:09:26 +0530

    first

---
+++ components/index.ts
@@ -0,0 +1,12 @@
+import Hero from './Hero';
+import Navbar from './Navbar';
+import Footer from './Footer';
+import ShowMore from './ShowMore';
+import SearchBar from './SearchBar';
+import CarCard from './CarCard';
+import CarDetails from './CarDetails';
+import SearchManufacturer from './SearchManufacturer';
+import CustomFilter from './CustomFilter';
+import CustomButton from './CustomButton';
+
+export {Hero,CustomButton,Navbar,Footer,SearchBar,CustomFilter,SearchManufacturer,CarCard,CarDetails,ShowMore};
